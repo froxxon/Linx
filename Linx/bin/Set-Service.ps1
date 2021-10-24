3b322592d03fd4129da273f724541637f439c425
froxxon <20445155+froxxon@users.noreply.github.com>
2021-10-24 15:19:24 +0200

    Add files via upload

--- Linx/bin/Set-Service.ps1
+++ Linx/bin/Set-Service.ps1
@@ -1,20 +1,14 @@
-﻿$nssm = "C:\Program Files\NSSM\nssm.exe"
-$serviceName = 'Linx'
-$powershell = (Get-Command powershell).Source
-$scriptPath = 'C:\RestPS\Linx\Start-Service.ps1'
-$arguments = "-ExecutionPolicy Bypass -File `"$scriptPath`""
+﻿param (
+    $NSSM           = 'C:\Program Files\NSSM\nssm.exe',
+    $ServiceName    = 'Linx',
+    $PowershellPath = ((Get-Command powershell).Source),
+    $LinxPath     = 'C:\RestPS\Linx\Start-Service.ps1'
+)
+$Arguments = '-ExecutionPolicy Bypass -File "$' + $LinxPath + '"'
+
 & $nssm install $serviceName $powershell $arguments
 & $nssm status $serviceName
-Start-Service $serviceName
-Get-Service $serviceName
+Get-Service $ServiceName
 
-#& $nssm edit $serviceName
-#& $nssm remove $serviceName confirm
-
-$gMSAAccount = 'gmsa-linx'
-install-adserviceaccount $gMSAAccount
-test-adserviceaccount $gMSAAccount
-
-#netsh http add sslcert ipport=192.168.1.24:443 certhash=CERTHASH "appid={2a81d04e-f236-17a7-b13a-3180fb3d91a5}" certstorename=My disablelegacytls=Enable
-#netsh http show sslcert 
-#netsh http delete sslcert 192.168.1.24:443
+#& $nssm edit $ServiceName # display a GUI to edit the service if necessary
+#& $nssm remove $ServiceName confirm # remove the service
