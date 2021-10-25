26ba792ed815596f7c0d50654ede2176f865666b
froxxon <20445155+froxxon@users.noreply.github.com>
2021-10-25 16:18:11 +0200

    Update Start-Service.ps1

--- Linx/Start-Service.ps1
+++ Linx/Start-Service.ps1
@@ -1,11 +1,11 @@
-﻿$global:ScriptVariables = @{ ScriptPath = ([regex]::match($PSCommandPath,".*(?:\\)")).Value }
+$global:ScriptVariables = @{ ScriptPath = ([regex]::match($PSCommandPath,".*(?:\\)")).Value }
 if ( $psISE ) { $ScriptVariables.ScriptPath = ([regex]::match($psISE.CurrentFile.FullPath,".*(?:\\)")).Value }
 import-module "$($ScriptVariables.ScriptPath)\modules\CustomizedRestPS.psm1" -force
 import-module "$($ScriptVariables.ScriptPath)\modules\Internal-CmdLets.psm1" -force
 
 $RestPSparams = @{
             RestPSLocalRoot  = $ScriptVariables.ScriptPath
-            RoutesFilePath   = "$($ScriptVariables.ScriptPath)\endpoints\Routes.json"
+            RoutesFilePath   = "$($ScriptVariables.ScriptPath)endpoints\Routes.json"
             LogFile          = $ScriptVariables.LogRestPSPath
             LogLevel         = 'Info'
             Port             = $ScriptVariables.Port
