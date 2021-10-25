bc5736b8f3835bc69f81196ea9eff4dd0f658df4
froxxon <20445155+froxxon@users.noreply.github.com>
2021-10-25 16:06:10 +0200

    Update Set-Service.ps1

--- Linx/bin/Set-Service.ps1
+++ Linx/bin/Set-Service.ps1
@@ -1,13 +1,13 @@
-﻿param (
+param (
     $NSSM           = 'C:\Program Files\NSSM\nssm.exe',
     $ServiceName    = 'Linx',
     $PowershellPath = ((Get-Command powershell).Source),
-    $LinxPath     = 'C:\RestPS\Linx\Start-Service.ps1'
+    $LinxPath       = 'C:\RestPS\Linx\Start-Service.ps1'
 )
 $Arguments = '-ExecutionPolicy Bypass -File "$' + $LinxPath + '"'
 
-& $nssm install $serviceName $powershell $arguments
-& $nssm status $serviceName
+& $nssm install $ServiceName $PowershellPath $Arguments
+& $nssm status $ServiceName
 Get-Service $ServiceName
 
 #& $nssm edit $ServiceName # display a GUI to edit the service if necessary
